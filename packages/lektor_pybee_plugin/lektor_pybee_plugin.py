fc5edc0b7069b5a3c0cf39d535e2b087abed5ea6
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-09-27 19:28:13 -0400

    Update plugin to include a is_alt_outdated method

--- packages/lektor_pybee_plugin/lektor_pybee_plugin.py
+++ packages/lektor_pybee_plugin/lektor_pybee_plugin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """This is a custom local plugin to ad extra functionality to pybee site."""
 # Standrd library imports
+import os
 import urllib
 
 # Third party imports
@@ -15,21 +16,34 @@ class PyBeePlugin(Plugin):
 
     def on_setup_env(self, **extra):
 
-        def is_alt_content_available(record):
+        def is_alt_outdated(record):
             """Checks if record path (content+<alt>.lr) file exists."""
             if not isinstance(record, Record):
                 raise ValueError('Must provide a `Record` object')
 
-            alt_available = True
             config = self.env.jinja_env.globals['config']
-            if record.alt != config.primary_alternative:
-                try:
-                    record.contents.as_text()
-                except IOError:
-                    # This means the alt content file does not exist
-                    alt_available = False
+            site = self.env.jinja_env.globals['site']
+            primary_path = site.get(record.path).contents.filename
+            primary_path = primary_path.replace(
+                '+{0}.lr'.format(config.primary_alternative), '.lr')
+            try:
+                alt_path = record.contents.filename
+                alt_text = '+{0}.lr'.format(config.primary_alternative)
+                if alt_text in alt_path:
+                    alt_path = alt_path.replace(alt_text, '.lr')
+            except IOError:
+                # This means the alt content file does not exist
+                alt_path = None
 
-            return alt_available
+            primary_modtime = None
+            if os.path.isfile(primary_path):
+                primary_modtime = os.path.getmtime(primary_path)
+
+            alt_modtime = None
+            if os.path.isfile(alt_path):
+                alt_modtime = os.path.getmtime(alt_path)
+
+            return primary_modtime > alt_modtime
 
         def urlencode_limit(string, limit=6000):
             """Encodes url for uri but if over limit returns None"""
@@ -45,7 +59,6 @@ class PyBeePlugin(Plugin):
 
             return string
 
-        self.env.jinja_env.globals.update(
-            is_alt_content_available=is_alt_content_available
-        )
+        self.env.jinja_env.globals.update(is_alt_outdated=is_alt_outdated)
         self.env.jinja_env.globals.update(urlencode_limit=urlencode_limit)
+        self.env.jinja_env.globals.update(dir=dir)
