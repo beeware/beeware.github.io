f8aaffce990f652614a81305208f7af3b5ffe1ce
dzc0d3r <m4dcod3r@protonmail.com>
2020-04-08 18:17:24 +0100

    Use in-repository updated copy of lektor-atom plugin instead of lektor-atom v0.3  which uses a deprecated Werkzeug's atom api

---
+++ packages/lektor_atom_plugin/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+This is a custom local lektor-atom plugin .
+"""
+
+from setuptools import setup
+
+setup(
+    author=u'A. Jesse Jiryu Davis',
+    author_email='jesse@emptysquare.net',
+    install_requires=['MarkupSafe', 'feedgenerator'],
+    keywords='Lektor plugin static-site blog atom rss',
+    license='MIT',
+    name='lektor-atom',
+    py_modules=['lektor_atom'],
+    url='https://github.com/nixjdm/lektor-atom',
+    version='0.4.0',
+    classifiers=[
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Framework :: Lektor',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'lektor.plugins': [
+            'atom = lektor_atom:AtomPlugin',
+        ]
+    }
+)
