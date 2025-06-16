f969d5e7e42df6c04f817eb4810e2671260f78f4
Charles Whittington <CharlesWhitt@gmail.com>
2025-06-16 03:18:48 -0400

    Add translation filter (#667)
    Simplify translations by using a jinja filter rather than a macro.

--- packages/lektor_beeware_plugin/lektor_beeware_plugin.py
+++ packages/lektor_beeware_plugin/lektor_beeware_plugin.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
-"""This is a custom local plugin to add extra functionality to BeeWare site."""
+"""This is a custom local plugin to add extra functionality to the BeeWare site."""
 
 from datetime import date
 
 from lektor.pluginsystem import Plugin
+from jinja2 import pass_context
+
+
+@pass_context
+def translate(context, string, bag_name="translate"):
+    # Make sure that any macros which need to call this are imported with context.
+    alt = context["this"].alt
+    bag = context["bag"]
+    if trans := bag(f"{bag_name}.{alt}.{string}"):
+        return trans
+
+    if en := bag(f"{bag_name}.en.{string}"):
+        return en
+
+    return ""
 
 
 class BeeWarePlugin(Plugin):
-    name = 'BeeWare Custom Lektor Plugin'
-    description = 'This is a custom local plugin to add extra functionality.'
+    name = "BeeWare Custom Lektor Plugin"
+    description = "This is a custom local plugin to add extra functionality."
 
     def on_setup_env(self, **extra):
-        self.env.jinja_env.globals['today'] = date.today()
+        self.env.jinja_env.globals["today"] = date.today()
+        self.env.jinja_env.filters["trans"] = translate
