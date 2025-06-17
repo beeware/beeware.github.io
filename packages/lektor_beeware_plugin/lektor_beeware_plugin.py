d5bd57b3b989eae9d6586e2c60c47f2e692406a2
Charles Whittington <charleswhitt@gmail.com>
2025-06-16 21:47:01 -0400

    Add link function, adjust sprint translation

--- packages/lektor_beeware_plugin/lektor_beeware_plugin.py
+++ packages/lektor_beeware_plugin/lektor_beeware_plugin.py
@@ -4,7 +4,8 @@
 from datetime import date
 
 from lektor.pluginsystem import Plugin
-from jinja2 import pass_context
+from jinja2 import pass_context, Template
+from markupsafe import Markup
 
 
 @pass_context
@@ -12,13 +13,24 @@ def translate(context, string, bag_name="translate"):
     # Make sure that any macros which need to call this are imported with context.
     alt = context["this"].alt
     bag = context["bag"]
+    result = ""
     if trans := bag(f"{bag_name}.{alt}.{string}"):
-        return trans
+        result = trans
 
-    if en := bag(f"{bag_name}.en.{string}"):
-        return en
+    if not result and (en := bag(f"{bag_name}.en.{string}")):
+        result = en
 
-    return ""
+    template = Template(result)
+    
+    # Wrapping it in Markup() marks it as safe, so it won't be escaped.
+    return Markup(template.render(context))
+
+
+def link_to(address):
+    def link_maker(text):
+        return f'<a href="{address}">{text}</a>'
+
+    return link_maker
 
 
 class BeeWarePlugin(Plugin):
@@ -27,4 +39,5 @@ class BeeWarePlugin(Plugin):
 
     def on_setup_env(self, **extra):
         self.env.jinja_env.globals["today"] = date.today()
+        self.env.jinja_env.globals["link_to"] = link_to
         self.env.jinja_env.filters["trans"] = translate
