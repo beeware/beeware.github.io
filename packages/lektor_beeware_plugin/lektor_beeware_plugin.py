a21b22d18d7c900a3deed572e097b90a5488926b
John Zhou <john-xyz@outlook.com>
2025-07-08 08:56:44 -0500

    Remove workaround

--- packages/lektor_beeware_plugin/lektor_beeware_plugin.py
+++ packages/lektor_beeware_plugin/lektor_beeware_plugin.py
@@ -13,20 +13,11 @@ import json
 def languagename(context, string):
     return context["bag"](f"languages.langs.{string}")
 
-def from_json(val):
-    return json.loads(val)
-
-def to_json(val):
-    return json.dumps(val)
-
-
 class BeeWarePlugin(Plugin):
     name = "BeeWare Custom Lektor Plugin"
     description = "This is a custom local plugin to add extra functionality."
 
     def on_setup_env(self, **extra):
         self.env.jinja_env.globals["today"] = date.today()
-        self.env.jinja_env.filters["from_json"] = from_json
-        self.env.jinja_env.filters["to_json"] = to_json
         self.env.jinja_env.filters["languagename"] = languagename
 
