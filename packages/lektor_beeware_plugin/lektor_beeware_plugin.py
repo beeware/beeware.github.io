e6f6453a520cb28b3d52a536f47669739741dfc4
Kattni <kattni@kattni.com>
2025-10-24 23:25:47 -0400

    Update CoC location, beeware_plugin to update CoC source.

--- packages/lektor_beeware_plugin/lektor_beeware_plugin.py
+++ packages/lektor_beeware_plugin/lektor_beeware_plugin.py
@@ -5,6 +5,8 @@ from datetime import date
 
 from lektor.pluginsystem import Plugin
 from jinja2 import pass_context
+import markdown
+import requests
 
 
 @pass_context
@@ -21,6 +23,23 @@ def translate(context, string, bag_name="translate"):
     return ""
 
 
+@pass_context
+def raw_github(context, path, repo, include_title=True):
+    """
+    Pull Markdown content from a raw-content GitHub link, convert it to HTML for
+    inclusion in a Lektor template, using `raw_github` in the template.
+
+    :param context: Lektor context.
+    :param path: The path to the markdown file relative to the root of the GitHub repo.
+    :param repo: The GitHub repo name.
+    """
+    raw_url = f"https://raw.githubusercontent.com/beeware/{repo}/refs/heads/main/{path}"
+    content = requests.get(raw_url).text
+    if not include_title:
+        _, content = content.split("\n", 1)
+    return markdown.markdown(content)
+
+
 class BeeWarePlugin(Plugin):
     name = "BeeWare Custom Lektor Plugin"
     description = "This is a custom local plugin to add extra functionality."
@@ -28,3 +47,4 @@ class BeeWarePlugin(Plugin):
     def on_setup_env(self, **extra):
         self.env.jinja_env.globals["today"] = date.today()
         self.env.jinja_env.filters["trans"] = translate
+        self.env.jinja_env.filters["raw_github"] = raw_github
